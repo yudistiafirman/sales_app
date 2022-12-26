553027f67586a2b86612dce779006a61bf56d3c1
Yudistia Firman <firman.yudistia@codemi.co.id>
2022-12-26 08:38:55 +0700

    SA-37/As FE I can create Pricelist navigation and screen

---
+++ src/screens/Price/PriceList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const PriceList = () => {
+  return (
+    <View>
+        <Text>this is price list</Text>
+    </View>
+  )
+}
+
+export default PriceList
