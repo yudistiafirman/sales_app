adb2b1d7bdd02d21af3f580a3503d22e0656c77b
Abi Raditya <abi.raditya@brik.id>
2022-12-31 10:36:23 +0700

    feature/SA-homepage/SA-83 I can create global component bottom sheet
    - install bottom sheet and it's dependancies

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,16 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      require.resolve("babel-plugin-module-resolver"),
+      require.resolve('babel-plugin-module-resolver'),
       {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@': './src',
-        }
+        },
       },
     ],
+    'react-native-reanimated/plugin', // Reanimated plugin has to be listed last.
   ],
   env: {
     production: {
