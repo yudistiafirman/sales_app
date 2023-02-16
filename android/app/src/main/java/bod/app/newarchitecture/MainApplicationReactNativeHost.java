05eb4a8bd190bac33703398382780870e73d3ea9
Martin <martin.luter@brik.id>
2023-02-16 09:10:50 +0700

    change: application id

--- android/app/src/main/java/bod/app/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/bod/app/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.bsa.newarchitecture;
+package bod.app.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.fabric.ReactNativeConfig;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.bsa.BuildConfig;
-import com.bsa.newarchitecture.components.MainComponentsRegistry;
-import com.bsa.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import bod.app.BuildConfig;
+import bod.app.newarchitecture.components.MainComponentsRegistry;
+import bod.app.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
