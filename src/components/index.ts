bdc32d6b2211562a743b3d7855c999d88ebe35bd
Laptop BRIK <User@Techs-MacBook-Pro.local>
2023-03-07 13:15:09 +0700

    remove unused component

--- src/components/index.ts
+++ src/components/index.ts
@@ -58,7 +58,6 @@ export { default as BPicList } from './organism/BPicList';
 export { default as BBottomSheetForm } from './organism/BBottomSheetForm';
 export { default as BAlert } from './organism/BAlert';
 export { default as BTabViewScreen } from './organism/BTabViewScreen';
-export { default as BCommonCompanyList } from './organism/BCommonCompanyList';
 export { default as BCalendar } from './organism/BCalendar';
 export { default as BNestedProductCard } from './organism/BNestedProductCard';
 export { default as BEmptyState } from './organism/BEmptyState';
