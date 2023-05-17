b81910534348a87085b6287a8b2c936ac26fbec9
Jajang Mulyana <firman.yudistia@brik.id>
2023-05-17 15:22:21 +0700

    change Sales Header Menu Right

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -9,7 +9,6 @@ import modalReducer from './reducers/modalReducer';
 import productivityFlowReducer from './reducers/productivityFlowReducer';
 import orderReducer from './reducers/orderReducer';
 import snackbarReducer from './reducers/snackbarReducer';
-import customerReducer from './reducers/customerReducer';
 import { createXStateMiddleware } from './middleware/createXStateMiddleware';
 import purchaseOrderReducer, {
   purchaseOrderSlice,
@@ -39,7 +38,6 @@ const rootReducer = combineReducers({
   common: commonReducer,
   order: orderReducer,
   snackbar: snackbarReducer,
-  customer: customerReducer,
   purchaseOrder: purchaseOrderReducer,
   operation: persistReducer<operationInitState, any>(
     persistConfig,
