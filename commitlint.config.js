269fa2f523bf44a4a3015f7f10faac6c127b16c7
Martin <martin.luter@brik.id>
2023-05-22 11:37:50 +0700

    change: prettier config same with fe web

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'add',
-      ],
-    ],
-  },
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "build",
+                "chore",
+                "ci",
+                "docs",
+                "feat",
+                "fix",
+                "perf",
+                "refactor",
+                "revert",
+                "style",
+                "test",
+                "add",
+                "change"
+            ]
+        ]
+    }
 };
