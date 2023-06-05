f780ccbe2910c7cd39e7e48c516b5385be03651f
Martin <martin.luter@brik.id>
2023-06-05 16:24:15 +0700

    remove: console log

--- src/machine/transactionMachine.tsx
+++ src/machine/transactionMachine.tsx
@@ -211,16 +211,12 @@ const transactionMachine =
                         loadTab: false
                     };
                 }),
-                assignIndexToContext: assign((_context, event) => {
-                    console.log("lolo 1:: ", event.payload);
-                    console.log("lolo 2:: ", _context.selectedType);
-                    return {
-                        selectedType: event.payload,
-                        page: 1,
-                        loadTransaction: true,
-                        transactionData: []
-                    };
-                }),
+                assignIndexToContext: assign((_context, event) => ({
+                    selectedType: event.payload,
+                    page: 1,
+                    loadTransaction: true,
+                    transactionData: []
+                })),
                 incrementPage: assign((context, _event) => ({
                     page: context.page + 1,
                     isLoadMore: true
@@ -267,8 +263,6 @@ const transactionMachine =
                     }
                 },
                 getTransactions: async (_context, _event) => {
-                    console.log("test 1:: ", _context.selectedType);
-                    console.log("test 2:: ", _event.type);
                     try {
                         let response;
                         if (_context.selectedType === "PO") {
