c19eb148dbeb5d9029dac98df1a762e1103cc159
Jajang Mulyana <firman.yudistia@brik.id>
2023-07-17 17:45:42 +0700

    chore: reset appointment state when finished makin appointment

--- src/screens/Appointment/index.tsx
+++ src/screens/Appointment/index.tsx
@@ -259,6 +259,7 @@ export default function Appointment() {
                         outsideClickClosePopUp: false,
                         outlineBtnAction: () => {
                             navigation.goBack();
+                            dispatch(resetAppointmentState());
                             dispatch(closePopUp());
                         },
                         primaryBtnAction: () => {
